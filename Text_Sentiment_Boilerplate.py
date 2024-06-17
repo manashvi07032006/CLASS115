6dd2d84677d7b52862d15031387ee10fbfd5f8d0
manashvi07032006 <80456520+manashvi07032006@users.noreply.github.com>
2024-06-17 20:03:30 +0530

    Add files via upload

---
+++ Text_Sentiment_Boilerplate.py
@@ -0,0 +1,20 @@
+import tensorflow.keras
+import numpy as np
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+sentence = ["I am happy to meet my friends. We are planning to go a party.", 
+            "I had a bad day at school. i got hurt while playing football"]
+
+# Tokenization
+
+# Create a word_index dictionary
+
+# Padding the sequence
+
+# Define the model using .h5 file
+
+# Test the model
+
+# Print the result
+
